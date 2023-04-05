5fc57836a64b4b90ee63642df10d16379d06d7dd
Arafat Amin <arafat15-5321@diu.edu.bd>
2023-04-05 11:38:20 +0600

    Seeding for Super User

---
+++ src/ShopManagement/Infrastructure/Seeds/AccountSeed.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Seeds
+{
+    internal class AccountSeed
+    {
+        PasswordHasher<ApplicationUser> password = new PasswordHasher<ApplicationUser>();
+
+        internal ApplicationUser user
+        {
+            get
+            {
+                return new ApplicationUser
+                {
+                    Id = Guid.NewGuid(),
+                    UserName = "Admin",
+                    Email = "super@admin.com",
+                    LockoutEnabled = false,
+                    PhoneNumber = "01521111111",
+                    PasswordHash = password.HashPassword(user, "admin123")
+                };
+            }
+        }
+    }
+}
