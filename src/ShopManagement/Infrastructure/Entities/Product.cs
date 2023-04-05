1d2b3b4bbc590c4cb2d8b2b5b37af7c8834d4278
Arafat Amin <arafat15-5321@diu.edu.bd>
2023-04-05 23:30:12 +0600

    Essential Entities

--- src/ShopManagement/Infrastructure/Entities/Product.cs
+++ src/ShopManagement/Infrastructure/Entities/Product.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Entities
 {
-    public class Product
+    public class Product : IEntity<int>
     {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int ProductCode { get; set; }
+        public int ParentCode { get; set; }
+        public int ProductBarCode { get; set; }
+        public string Description { get; set; }
+        public Brand Brand { get; set; }
+        public int BrandId { get; set; }
+        public int SizeId { get; set; }
+        public int ColorId { get; set; }
+        public string ImagePath { get; set; }
 
     }
 }
---
+++ src/ShopManagement/Infrastructure/Entities/ProductDetail.cs
@@ -0,0 +1,21 @@
+﻿namespace Infrastructure.Entities
+{
+    public class ProductDetail : IEntity<int>
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int CatagoryId { get; set; }
+        public int UnitId { get; set; }
+        public List<string> WarehouseList { get; set; }
+        public int Stock { get; set; }
+        public int TotalPurchase { get; set; }
+        public DateOnly LastPurchaseDate { get; set; }
+        public string LastPurchaseSupplier { get; set; }
+        public int TotalSales { get; set; }
+        public DateOnly LastSaleDate { get; set; }
+        public string LastSaleCustomer { get; set; }
+        public string Type { get; set; }
+        public bool Status { get; set; }
+    }
+}
