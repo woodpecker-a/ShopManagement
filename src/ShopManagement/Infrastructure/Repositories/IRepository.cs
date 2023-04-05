40b6fcf5153d554675b835ef6439aebc0419126c
Arafat Amin <arafat15-5321@diu.edu.bd>
2023-04-05 22:55:07 +0600

    Repository Pattern

---
+++ src/ShopManagement/Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Infrastructure.Repositories
+{
+    public interface IRepository<TEntity, TKey> where TEntity : class, IEntity<TKey>
+    {
+        void Add(TEntity entity);
+        TEntity GetById(TKey id);
+        IList<TEntity> GetAll();
+        void Edit(TEntity entityToUpdate);
+        void Remove(TKey id);
+        void Remove(TEntity entityToDelete);
+    }
+}
