76acb38bc78a379a77bd66b573be963572ae8aa9
Arafat Amin <arafat15-5321@diu.edu.bd>
2023-04-06 14:24:06 +0600

    UnitOfWork layer for transection

---
+++ src/ShopManagement/Infrastructure/UnitOfWorks/ApplicationUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.DbContexts;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.UnitOfWorks
+{
+    public class ApplicationUnitOfWork : UnitOfWork, IApplicationUnitOfWork
+    {
+        public IProductRepository Product { get; private set; }
+
+        public ApplicationUnitOfWork(IApplicationDbContext db, IProductRepository product) : base((DbContext)db)
+        {
+            Product = product;
+        }
+    }
+}
